d325e1a3c6596193e07b3b71e87554f541461c59
g-akca <gok.akca66@gmail.com>
2026-04-06 21:11:41 +0300

    Add SocialMediaCard structure and styles

--- src/components/SocialMediaCard.jsx
+++ src/components/SocialMediaCard.jsx
@@ -1,7 +1,15 @@
+import starsImg from "/images/illustration-five-stars.webp";
+
 function SocialMediaCard() {
   return (
-    <>
-    </>
+    <div className="py-10 px-8 bg-purple-500 rounded-[10px] text-white text-center flex flex-col gap-6">
+      <h1 className="text-[46px] leading-10.5 font-medium tracking-[-4%]">Social Media <span className="text-yellow-500">10x</span> <i>Faster</i> with AI</h1>
+
+      <div className="flex flex-col gap-2 items-center">
+        <img src={starsImg} alt="Five stars" className="w-48" />
+        <p>Over 4,000 5-star reviews</p>
+      </div>
+    </div>
   )
 }
 
