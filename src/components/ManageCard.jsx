67b1ac305eabb098b79d067b2640a99cd4690170
g-akca <gok.akca66@gmail.com>
2026-04-07 11:56:51 +0300

    Add ManageCard structure and styles

--- src/components/ManageCard.jsx
+++ src/components/ManageCard.jsx
@@ -1,7 +1,12 @@
+import platformsImg from "/images/illustration-multiple-platforms.webp";
+
 function ManageCard() {
   return (
-    <>
-    </>
+    <div className="bg-white p-4 rounded-[10px] flex flex-col gap-4">
+      <img src={platformsImg} alt="Image of two accounts on different platforms" />
+
+      <h2 className="font-medium text-black text-[26px] leading-6 tracking-[-6%]">Manage multiple accounts and platforms.</h2>
+    </div>
   )
 }
 
