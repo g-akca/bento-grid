67b6a609fc905399c0b4aa7f057e3341fd8108b9
g-akca <gok.akca66@gmail.com>
2026-04-07 12:56:06 +0300

    Add AICard structure and styles

--- src/components/AICard.jsx
+++ src/components/AICard.jsx
@@ -1,7 +1,12 @@
+import aiImg from "/images/illustration-ai-content.webp";
+
 function AICard() {
   return (
-    <>
-    </>
+    <div className="bg-yellow-500 p-6 rounded-[10px] flex flex-col gap-6">
+      <h2 className="text-[32px] leading-7 tracking-[-6%] font-medium">Write your content using AI.</h2>
+
+      <img src={aiImg} alt="AI chat image" className="w-55" />
+    </div>
   )
 }
 
