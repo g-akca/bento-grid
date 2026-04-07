8ace67500c99634e4f5056da4adf747445dbed2d
g-akca <gok.akca66@gmail.com>
2026-04-07 12:39:47 +0300

    Add FollowerCard structure and styles

--- src/components/FollowerCard.jsx
+++ src/components/FollowerCard.jsx
@@ -1,7 +1,12 @@
+import followerImg from "/images/illustration-grow-followers.webp";
+
 function FollowerCard() {
   return (
-    <>
-    </>
+    <div className="bg-purple-500 p-6 rounded-[10px] flex flex-col gap-10 items-center text-center">
+      <img src={followerImg} alt="Grow followers" className="w-56.75" />
+
+      <h2 className="text-white font-medium text-[32px] leading-7 tracking-[-6%]">Grow followers with non-stop content.</h2>
+    </div>
   )
 }
 
